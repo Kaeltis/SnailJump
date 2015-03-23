f9fedfd5096808154d988cd1249ac2adde2f2279
Patrick Fruh <info@kaeltis.de>
2015-03-23 13:16:50 +0100

    death music

--- js/game.js
+++ js/game.js
@@ -173,6 +173,7 @@ function render() {
 }
 
 function gameOver(score) {
+    dieMusic.play();
     alert("Game Over!");
     if (getCookie('highscore') < score) {
         name = prompt("Neuer Highscore!\nBitte Namen eingeben:", "");
