3ee3865a3adf00bb0d18c45975fcf5f3f5a79586
Patrick Fruh <info@kaeltis.de>
2015-03-23 11:07:05 +0100

    self moving camera and out of camera detection

--- js/game.js
+++ js/game.js
@@ -28,7 +28,6 @@ var vertMove = -180; // jump
 var jumpTimer = 0;
 
 function create() {
-
     //Background
     game.stage.backgroundColor = '#FFFFFF';
     game.add.tileSprite(0, 0, 2000, 600, 'background');
@@ -59,7 +58,7 @@ function create() {
         'p1_walk07.png',
         'p1_walk08.png',
         'p1_walk09.png'
-    ], 30, true, false);
+    ], 30, false, false);
 
 
     // Player Physics
@@ -67,36 +66,47 @@ function create() {
     player.body.bounce.y = 0.1;
     player.body.gravity.y = 160;
 
-    // Camera
-    game.camera.follow(player);
-
     // Controls
     cursors = game.input.keyboard.createCursorKeys();
     jumpButton = game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
 
+    // Set Camera
+    game.camera.x = player.body.x - 150;
+    game.camera.y = player.body.y;
 }
 
 function update() {
-    // Check collisions
+    // Check collisions & Move player forward
     game.physics.arcade.collide(player, layer);
+    player.body.velocity.x = 10;
 
-    player.body.velocity.x = 0;
+    // Update Camera
+    game.camera.y = player.body.y;
+    game.camera.x = game.time.now / 100;
+    if (!player.inCamera) {
+        playerOOC();
+    }
+
 
     if (cursors.left.isDown) {
         player.body.velocity.x = -hozMove;
-        player.animations.play('walk');
+        player.animations.play('walk', 30);
     }
     else if (cursors.right.isDown) {
         player.body.velocity.x = hozMove;
-        player.animations.play('walk');
+        player.animations.play('walk', 30);
     }
     else {
-        player.animations.stop();
+        player.animations.play('walk', 10);
     }
 
     if (jumpButton.isDown && player.body.onFloor() && game.time.now > jumpTimer) {
         player.body.velocity.y = vertMove;
         jumpTimer = game.time.now + 650;
     }
+}
 
+function playerOOC() {
+    alert("Player OOC!");
+    location.reload();
 }
